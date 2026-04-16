a65ce92cd034cc6cee5816f72767a3b740154c2a
Salah Adel <salahadel20003@gmail.com>
2026-04-16 23:35:32 +0200

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,16 @@ const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 
-const SECRET = process.env.JWT_SECRET || "mysecretkey";
-
-const ADMIN_USER = process.env.ADMIN_USER || "SALAH";
-const ADMIN_PASS = process.env.ADMIN_PASS || "SALAHB5a-";
+const SECRET = process.env.JWT_SECRET;
+const ADMIN_USER = process.env.ADMIN_USER;
+const ADMIN_PASS = process.env.ADMIN_PASS;
 
 router.post("/login", (req, res) => {
   const { username, password } = req.body;
+
+  if (!SECRET || !ADMIN_USER || !ADMIN_PASS) {
+    return res.status(500).json({ message: "Server config error" });
+  }
 
   if (username === ADMIN_USER && password === ADMIN_PASS) {
     const token = jwt.sign({ user: username }, SECRET, {
